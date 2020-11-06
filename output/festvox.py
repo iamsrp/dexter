a052f879dd88291b56327fe3074e8cf61efef8c8
Steve Payne <srp@iamsrp.com>
2020-11-05 23:44:58 -0800

    Added pyfestival support
    It seems that this _kinda_ works so we put it in for now but I wouldn't
    recommend using it in its current state.

--- output/festvox.py
+++ output/festvox.py
@@ -12,6 +12,7 @@ from   dexter.core.log import LOG
 from   dexter.output   import SpeechOutput
 from   threading       import Thread
 
+import re
 import select
 import subprocess
 import time
@@ -20,10 +21,7 @@ import time
 
 class FestivalOutput(SpeechOutput):
     """
-    An output which logs as a particular level to the system's log.
-
-    We run this in a subprocess since the in-process version tends to lock
-    things up and also doesn't work outside the main thread.
+    A speech to text output using Festival in a subprocess.
     """
     def __init__(self,
                  state,
@@ -108,7 +106,7 @@ class FestivalOutput(SpeechOutput):
                 text  = self._queue.pop()
                 text  = text.replace('"', '')
 
-                # Ignore empty strings since they confuse Festival
+                # Ignore empty strings
                 if not text:
                     LOG.info("Nothing to say...")
                     continue
@@ -146,3 +144,148 @@ class FestivalOutput(SpeechOutput):
                 self._subproc.communicate()
         except:
             pass
+
+
+# ----------------------------------------------------------------------
+
+
+class PyFestivalOutput(SpeechOutput):
+    """
+    A speech to text output which uses the pyfestival module.
+
+    You might be able to pip install it, but I had to build it by hand using
+    source from ``https://github.com/techiaith/pyfestival`` to make it work.
+
+    Note that pyfestival seems to hold on to the GIL while it's running and so
+    none of the rest of the system can do anything. This isn't really great
+    either.
+    """
+    def __init__(self,
+                 state,
+                 voice='voice_cmu_us_slt_arctic_hts'):
+        """
+        @see Output.__init__()
+        :type  voice: str
+        :param voice:
+            The voice to use.
+        """
+        super(PyFestivalOutput, self).__init__(state)
+
+        self._voice         = voice
+        self._queue         = []
+        self._interrupted   = False
+        self._boot_strapped = False
+        self._start_error   = None
+
+
+    def write(self, text):
+        """
+        @see Output.write
+        """
+        if text is not None:
+            self._queue.append(str(text))
+
+
+    def interrupt(self):
+        """
+        @see Output.interrupt
+        """
+        self._interrupted = True
+
+
+    def _start(self):
+        """
+        @see Component._start()
+        """
+        # Say that we're setting things up, since we might pause for a bit doing
+        # it etc.
+        self._notify(Notifier.INIT)
+
+        # Now spawn the worker thread
+        thread = Thread(target=self._run)
+        thread.daemon = True
+        thread.start()
+
+        # Wait for the thread to become properly active
+        start = time.time()
+        while not self._boot_strapped and thread.is_alive():
+            if time.time() - start > 10:
+                raise TimeoutError(
+                    "Timed out waiting for worker thread to start"
+                )
+            time.sleep(0.1)
+
+        # Back to idle when we're done either way
+        self._notify(Notifier.IDLE)
+
+        # What state is it in?
+        if not thread.is_alive():
+            raise ValueError(
+                "Failed to start worker thread: %s: " %
+                (self._start_error if self._start_error else "Unknown error")
+            )
+
+
+    def _stop(self):
+        """
+        @see Component._stop()
+        """
+        # Clear any pending dialogue
+        self._queue = []
+
+
+    def _run(self):
+        """
+        The actual worker thread.
+        """
+        # Festival is a little hokey and so we need to start it in the same
+        # thread that we use it. Otherwise we get an message saying:
+        #  SIOD ERROR: the currently assigned stack limit has been exceeded
+        # As such we need to do everything here and communicate back success or
+        # failure to the _start() method via member variables. Lovely.
+        try:
+            import festival
+            festival.execCommand(self._voice)
+            self._boot_strapped = True
+        except Exception as e:
+            self._start_error = e
+            return
+
+        # Keep going until we're told to stop
+        while self.is_running:
+            if len(self._queue) == 0:
+                time.sleep(0.1)
+                continue
+
+            # Else we have something to say
+            try:
+                # Get the text, make sure that '"'s in it won't confuse things
+                start = time.time()
+                text  = self._queue.pop()
+                text  = text.replace('"', '')
+
+                # Ignore empty strings
+                if not text:
+                    LOG.info("Nothing to say...")
+                    continue
+
+                # We're about to say something, clear any interrupted flag ready
+                # for any new one
+                self._interrupted = False
+
+                # We're talking so mark ourselves as active accordingly
+                self._notify(Notifier.WORKING)
+
+                # Break up the text into bits and say them so that we can
+                # interrupt the output. Then say each part. We break on natural
+                # pauses in the speech.
+                for part in re.split(r'[\.,;:]', text):
+                    if self._interrupted:
+                        break
+                    festival.sayText(part)
+
+            except Exception as e:
+                LOG.error("Failed to say '%s': %s" % (text, e))
+
+            finally:
+                self._notify(Notifier.IDLE)
