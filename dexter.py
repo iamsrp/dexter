33db6066261434bd9ac99996303906fb9ffaa740
Steve Payne <github@iamsrp.com>
2022-10-22 14:52:20 -0700

    Deeply replace environment variables
    Go into container types when replacing environment variables. This handles lists
    etc. now which some services might employ.

--- dexter.py
+++ dexter.py
@@ -54,6 +54,72 @@ CONFIG = {
 
 # ------------------------------------------------------------------------------
 
+def _replace_envvars(value):
+    """
+    Replace any environment variables in the given type.
+
+    :return: A deep copy of the type with the values replaced.
+    """
+    if type(value) == str:
+        return _replace_envvars_str(value)
+    elif type(value) == tuple or type(value) == list:
+        return _replace_envvars_list(value)
+    elif type(value) == dict:
+        return _replace_envvars_dict(value)
+    else:
+        return value
+
+
+def _replace_envvars_str(value):
+    """
+    Look for environment variables in the form of ``${VARNAME}`` and replace them
+    with their values. This isn't overly pretty, but it works.
+    """
+    try:
+        while True:
+            # Pull out the variable name, if it exists
+            start    = value.index('${')
+            end      = value.index('}', start)
+            varname  = value[start+2:end]
+            varvalue = os.environ.get(varname, '')
+
+            # Special handling for some variables
+            if not varvalue:
+                # These are not always set in the environment but
+                # people tend to expect it to be, so we are nice and
+                # provide them
+                if varname == "HOSTNAME":
+                    varvalue = socket.gethostname()
+                elif varname == "USER":
+                    varvalue = getpass.getuser()
+
+            # And replace it
+            value = (value[:start] + varvalue + value[end+1:])
+    except:
+        # This means we failed to find the opening or closing
+        # varname container in the string, so we're done
+        pass
+    
+    # Give it back
+    return value
+
+
+def _replace_envvars_list(value):
+    try:
+        return [_replace_envvars(v) for v in value]
+    except:
+        return value
+
+
+def _replace_envvars_dict(value):
+    try:
+        return dict((k, _replace_envvars(v)) for (k,v) in value.items())
+    except:
+        return value
+
+
+# ------------------------------------------------------------------------------
+
 # Main entry point
 @argh.arg('--log-level', '-L',
           help="The logging level to use")
@@ -82,8 +148,7 @@ def main(log_level=None, config=None):
     else:
         configuration = CONFIG
 
-    # Handle environment variables in the component kwargs, in the form of
-    # "${VARNAME}". This isn't overly pretty, but it works.
+    # Handle environment variables in the component kwargs
     for typ in configuration['components']:
         # We might have kwargs for all the components
         for component in configuration['components'][typ]:
@@ -93,40 +158,15 @@ def main(log_level=None, config=None):
 
             # Look at all the kwargs which we have and check for environment
             # variables in the value names.
+            updated = _replace_envvars(kwargs)
             for name in kwargs:
-                value = kwargs[name]
-                try:
-                    while True:
-                        # Pull out the variable name, if it exists
-                        start    = value.index('${')
-                        end      = value.index('}', start)
-                        varname  = value[start+2:end]
-                        varvalue = os.environ.get(varname, '')
-
-                        # Special handling for some variables
-                        if not varvalue:
-                            # These are not always set in the environment but
-                            # people tend to expect it to be, so we are nice and
-                            # provide them
-                            if varname == "HOSTNAME":
-                                varvalue = socket.gethostname()
-                            elif varname == "USER":
-                                varvalue = getpass.getuser()
-
-                        # And replace it
-                        value = (value[:start] + varvalue + value[end+1:])
-                except:
-                    # This means we failed to find the opening or closing
-                    # varname container in the string, so we're done
-                    pass
-
                 # If we changed it then save it back in
-                if value != kwargs[name]:
+                if updated[name] != kwargs[name]:
                     LOG.info(
                         "Expanded component %s:%s:%s argument from '%s' to '%s'" %
-                        (typ, which, name, kwargs[name], value)
+                        (typ, which, name, kwargs[name], updated[name])
                     )
-                    kwargs[name] = value
+                    kwargs[name] = updated[name]
 
     # And spawn it
     dexter = Dexter(configuration)
